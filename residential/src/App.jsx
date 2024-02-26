cca50165ed6a82aa4be169fb3e1386ceddeee33f
antonnprince <antonprince95@gmail.com>
2024-02-26 22:16:04 +0530

    working

--- residential/src/App.jsx
+++ residential/src/App.jsx
@@ -2,20 +2,22 @@ import {Canvas} from "@react-three/fiber";
 import { Suspense } from "react";
 import Building from "./models/Building";
 import { Center } from "@react-three/drei";
+import Building1 from "./models/Building1";
 
 
 function App() {
   return(
-    <section className="w-full">
+    <section className="w-full h-screen relative bg-slate-500">
+    
       <Canvas 
-      className="w-full h-auto  bg-transparent cursor-grabbing"
+        className="w-full h-screen bg-transparent"
       >
-  
-         <directionalLight position={[1,1,1]} intensity={2} />
-          <ambientLight  intensity={0.5}/>
+         <directionalLight position={[1,1,1]} intensity={1} camera={{near:0.01, far: 1000}}/>
+          <ambientLight  intensity={0.25}/>
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
-          <Building  position={ [300, -500,-100]} scale={[0.5, 0.5, 0.5]}/>
+          <Building position={[0, -10,-10]} scale={[0.02, 0.02, 0.02]}/>
       </Canvas>
+   
     </section>
   )
 }
--- residential/src/Loader.jsx
+++ residential/src/Loader.jsx
@@ -6,6 +6,12 @@ export const Loader=()=>{
         <div className='flex justify-center items-center'>
             <div className='w-20 h-20 border-2 border-opacity-20 border-blue-500
              border-t-blue-500 rounded-full animate-spin '/>
+             
+      <h1 className="text-3xl text-black">TESTTT</h1>
+      <h1 className="text-3xl text-black">TESTTT</h1>
+      <h1 className="text-3xl text-black">TESTTT</h1>
+      <h1 className="text-3xl text-black">TESTTT</h1>
+      <h1 className="text-3xl text-black">TESTTT</h1>
         </div>
         </Html>
     )
