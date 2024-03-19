ac0fd38091b583e92a04f610cee0591595bb723d
antonnprince <antonprince95@gmail.com>
2024-03-19 22:15:41 +0530

    buikd version

--- residential/src/App.jsx
+++ residential/src/App.jsx
@@ -2,6 +2,7 @@ import {Canvas} from "@react-three/fiber";
 import { Suspense, useRef } from "react";
 import Building from "./models/Building";
 import Building1 from "./models/Building1";
+import { OrbitControls } from "@react-three/drei";
 
 
 function App() {
@@ -9,16 +10,18 @@ function App() {
   
   return(
     <section className="w-full h-screen relative bg-slate-500">
-    
+  
       <Canvas 
         className="w-full h-screen bg-transparent cursor-grab"
       >
-         <directionalLight position={[1,1,1]} intensity={1} camera={{near:0.01, far: 1000}}/>
+        <OrbitControls/>
+         <directionalLight position={[1,1,1]} intensity={1} />
           <ambientLight  intensity={0.25}/>
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
           <Building position={[0, -10,-10]} scale={[0.01, 0.01, 0.01]}/>
+          
       </Canvas>
-   
+    
     </section>
   )
 }
--- residential/src/models/Building.jsx
+++ residential/src/models/Building.jsx
@@ -1,102 +1,12 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import buildingScene from "../assets/building2.glb";
 import { a } from "@react-spring/three";
-import { useFrame, useThree } from "@react-three/fiber";
 
 const Building = (props) => {
   const buildref = useRef();
   const { nodes, materials } = useGLTF(buildingScene);
-  const [isRotating, setIsRotating] = useState(false);
 
-  const { gl } = useThree();
-
-  const lastX = useRef(0);
-  const rotationSpeed = useRef(0);
-  const dampingFactor = 0.95;
-
-  useEffect(() => {
-    const handlePointerDown = (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      setIsRotating(true);
-      lastX.current = event.touches ? event.touches[0].clientX : event.clientX;
-    };
-
-    const handlePointerUp = (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      setIsRotating(false);
-    };
-
-    const handlePointerMove = (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      if (isRotating) {
-        const clientX = event.touches
-          ? event.touches[0].clientX
-          : event.clientX;
-
-        const delta = (clientX - lastX.current) / window.innerWidth;
-
-        buildref.current.rotation.y += delta * 0.1 * Math.PI;
-
-        lastX.current = clientX;
-
-        rotationSpeed.current = delta * 0.1 * Math.PI;
-      }
-    };
-
-    const handleKeyDown = (event) => {
-      if (event.key === "ArrowLeft") {
-        if (!isRotating) setIsRotating(true);
-
-        buildref.current.rotation.y -= 0.01 * Math.PI;
-        rotationSpeed.current = -0.007;
-      } else if (event.key === "ArrowRight") {
-        if (!isRotating) setIsRotating(true);
-
-        buildref.current.rotation.y += 0.01 * Math.PI;
-        rotationSpeed.current = 0.007;
-      }
-    };
-
-    const handleKeyUp = (event) => {
-      if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
-        setIsRotating(false);
-      }
-    };
-
-    const canvas = gl.domElement;
-    canvas.addEventListener("pointerdown", handlePointerDown);
-    canvas.addEventListener("pointerup", handlePointerUp);
-    canvas.addEventListener("pointermove", handlePointerMove);
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      canvas.removeEventListener("pointerdown", handlePointerDown);
-      canvas.removeEventListener("pointerup", handlePointerUp);
-      canvas.removeEventListener("pointermove", handlePointerMove);
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [gl, isRotating]);
-
-  useFrame(() => {
-    if (!isRotating) {
-      rotationSpeed.current *= dampingFactor;
-
-      if (Math.abs(rotationSpeed.current) < 0.001) {
-        rotationSpeed.current = 0;
-      }
-
-      buildref.current.rotation.y += rotationSpeed.current;
-    
-    
-    
-    }
-  });
 
   return (
     <a.group {...props} dispose={null} ref={buildref} rotation={[-Math.PI/2,0,0]}>
